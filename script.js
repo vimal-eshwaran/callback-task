b65ac5dff834bb43996ae56628fd7790f8b285d3
vimal-eshwaran <118733978+vimal-eshwaran@users.noreply.github.com>
2023-06-26 22:37:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+ 
+ setTimeout(() => {
+     document.getElementById("main").innerHTML=10;
+     setTimeout(() => {
+        document.getElementById("main").innerHTML=9;
+        setTimeout(() => {
+            document.getElementById("main").innerHTML=8;
+            setTimeout(() => {
+                document.getElementById("main").innerHTML=7;
+                setTimeout(() => {
+                    document.getElementById("main").innerHTML=6;
+                    setTimeout(() => {
+                        document.getElementById("main").innerHTML=5;
+                        setTimeout(() => {
+                            document.getElementById("main").innerHTML=4;
+                            setTimeout(() => {
+                                document.getElementById("main").innerHTML=3;
+                                setTimeout(() => {
+                                    document.getElementById("main").innerHTML=2;
+                                    setTimeout(() => {
+                                        document.getElementById("main").innerHTML=1;
+                                        setTimeout(() => {
+                                            document.getElementById("main").innerHTML="Happy Independence day";
+ },1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
+},1000);
